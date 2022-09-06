c06b9877ace1c6e86f8e048d173781d161864f8f
RichardLemos <richardlemos@unigranrio.br>
2022-09-06 17:12:40 -0300

    Banco de dados H2 e criação automática da base de dados

--- src/main/java/com/nelioalves/cursomc/domain/Categoria.java
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -3,9 +3,17 @@ package com.nelioalves.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
@@ -15,7 +23,7 @@ public class Categoria implements Serializable {
 
 	public Categoria(Integer id, String nome) {
 		super();
-		this.id = id;
+		this.id = id; // Método Construtor
 		this.nome = nome;
 	}
 
@@ -46,7 +54,7 @@ public class Categoria implements Serializable {
 			return true;
 		if (obj == null)
 			return false;
-		if (getClass() != obj.getClass())
+		if (getClass() != obj.getClass()) // Permite comparar objetos pelo seu conteúdo
 			return false;
 		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id);
