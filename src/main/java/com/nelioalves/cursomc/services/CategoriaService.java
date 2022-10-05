d13b98547c24adcdfb524264c38e33e2b24b9f7d
RichardLemos <richardlemos@unigranrio.br>
2022-10-04 22:52:42 -0300

    PUT, DELETE E GET para Cliente

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -47,17 +47,17 @@ public class CategoriaService {
 		}
 	}
 
-	public List<Categoria> findAll() {
+	public List<Categoria> findAll() { // MÉTODO OBTER TODAS AS CATEGORIAS
 		return repo.findAll();
 	}
 
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		return repo.findAll(pageRequest);
+		return repo.findAll(pageRequest); // MÉTODO DE PAGINAÇÃO
 	}
 
 	public Categoria fromDTO(CategoriaDTO objDto) {
-		return new Categoria(objDto.getId(), objDto.getNome());
+		return new Categoria(objDto.getId(), objDto.getNome()); // MÉTODO DTO
 	}
 
 }
